e74c2081c50164bb3c352f9127c15e96b8bca64d
himani110 <himani110@gmail.com>
2020-12-01 12:52:12 +1100

    Update SignInPage.py

--- Pages/SignInPage.py
+++ Pages/SignInPage.py
@@ -7,38 +7,33 @@ from selenium.webdriver.support import expected_conditions as E
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 import utilities as U
-
-emailid = "hdummy030@gmail.com"
-password = "pass@123"
-term = "My Profile at Bunnings"
-Email_locator = "body_1_LoginForm_UserName"
-Password_locator = "body_1_LoginForm_Password"
-signin_button_locator = "LoginButton"
-message = "Exception Occurred"
-log_file = "../Logs/log.txt"
+import Constant as C
 
 
 class SignInPage:
+    wait_time_out = 5
 
     def __init__(self, drv):
         self.drv = drv
-        # self.wait_variable = W(self.drv, self.wait_time_out)
+        self.wait_variable = W(self.drv, self.wait_time_out)
+        drv.get(C.base_url)
+        drv.maximize_window()
 
     def Login(self):
 
         try:
             self.Click_SignIn()
             self.drv.find_element(By.ID, "body_1_LoginForm_UserName").click()
-            self.drv.find_element(By.ID, "body_1_LoginForm_UserName").sendkeys(emailid)
+            self.drv.find_element(By.ID, "body_1_LoginForm_UserName").sendkeys(C.emailid)
 
             self.drv.find_element(By.ID, "body_1_LoginForm_Password").click()
-            self.drv.find_element(By.ID, "body_1_LoginForm_Password").sendkeys(password)
+            self.drv.find_element(By.ID, "body_1_LoginForm_Password").sendkeys(C.password)
 
             self.drv.find_element(By.ID, "LoginButton").click()
             time.sleep(2)
-            self.wait_variable.until(E.title_contains(self.term))
+            self.wait_variable.until(E.title_contains(C.term))
         except NoSuchElementException:
-            U.log("INFO: ", message, log_file)
+            U.log("INFO: ", C.message, C.log_file)
             pass
 
     def Click_SignIn(self):
